7a2a81bd17652d64eccb0ab31578e45d0fba9ea2
zak905 <zakaria.amine88@gmail.com>
2022-06-20 20:40:36 +0200

    update Spring boot version and mongo interfaces in tests

--- src/main/java/com/gwidgets/mongotest/MongoTestApplication.java
+++ src/main/java/com/gwidgets/mongotest/MongoTestApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongoTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MongoTestApplication.class, args);
 	}
-
 }
 
--- src/test/java/com/gwidgets/mongotest/TransactionRepositoryTest.java
+++ src/test/java/com/gwidgets/mongotest/TransactionRepositoryTest.java
@@ -1,39 +1,49 @@
 package com.gwidgets.mongotest;
 
 
-import com.mongodb.MongoClientURI;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.mongodb.WriteConcern;
 import de.flapdoodle.embed.mongo.MongodExecutable;
 import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.support.MongoRepositoryFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataMongoTest(excludeAutoConfiguration= {EmbeddedMongoAutoConfiguration.class})
+@ContextConfiguration(classes = {TransactionRepositoryTest.MongoConfiguration.class})
+@EnableMongoRepositories
 public class TransactionRepositoryTest {
+
+    static MongodExecutable executable;
 
     @Autowired
     private TransactionRepository transactionRepository;
@@ -41,6 +51,11 @@ public class TransactionRepositoryTest {
     private final static List<String> USER_ID_LIST = Arrays.asList("b2b1f340-cba2-11e8-ad5d-873445c542a2", "bd5dd3a4-cba2-11e8-9594-3356a2e7ef10");
 
     private static final Random RANDOM = new Random();
+
+    @BeforeAll
+    public static void generalSetup() throws UnknownHostException, IOException {
+        configureAndStartEmbbededMongo();
+    }
 
 
     @BeforeEach
@@ -75,35 +90,35 @@ public class TransactionRepositoryTest {
         assertThat(resultsPage).extracting("success").allMatch(sucessfull -> (Boolean)sucessfull == true);
     }
 
+    @AfterAll
+    public static void tearDown() {
+        executable.stop();
+    }
+
+    private static void configureAndStartEmbbededMongo() throws UnknownHostException, IOException {
+        int port = 27019;
+
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+
+        MongodConfig mongodConfig = MongodConfig.builder()
+            .version(Version.Main.V4_0)
+            .net(new Net(port, Network.localhostIsIPv6()))
+            .build();
+        ;
+        executable = starter.prepare(mongodConfig);
+        executable.start();
+    }
+
     @Configuration
-    static class MongoConfiguration implements InitializingBean, DisposableBean {
-
-         MongodExecutable executable;
-
-        @Override
-        public void afterPropertiesSet() throws Exception {
-            String host = "localhost";
-            int port = 27019;
-
-            IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
-                    .net(new Net(host, port, Network.localhostIsIPv6()))
-                    .build();
-
-            MongodStarter starter = MongodStarter.getDefaultInstance();
-            executable = starter.prepare(mongodConfig);
-            executable.start();
-        }
-
+    static class MongoConfiguration {
 
         @Bean
-        public MongoDbFactory factory() {
-            MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(new MongoClientURI("mongodb://localhost:27019/test_db"));
-            return mongoDbFactory;
+        public MongoDatabaseFactory factory() {
+            return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27019/imager200_test");
         }
 
-
         @Bean
-        public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory) {
+        public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory) {
             MongoTemplate template = new MongoTemplate(mongoDbFactory);
             template.setWriteConcern(WriteConcern.ACKNOWLEDGED);
             return template;
@@ -115,11 +130,6 @@ public class TransactionRepositoryTest {
             mongoDbFactoryBean.setMongoOperations(template);
 
             return mongoDbFactoryBean;
-        }
-
-        @Override
-        public void destroy() throws Exception {
-            executable.stop();
         }
     }
 }
